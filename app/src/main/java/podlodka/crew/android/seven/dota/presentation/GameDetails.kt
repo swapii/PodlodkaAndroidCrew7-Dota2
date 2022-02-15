526b872efc94e33b6fdc7efcdc304c12faf3073b
Pavel Savinov <swapii@gmail.com>
2022-02-15 15:36:48 +0300

    Apply colors from design

--- app/src/main/java/podlodka/crew/android/seven/dota/presentation/GameDetails.kt
+++ app/src/main/java/podlodka/crew/android/seven/dota/presentation/GameDetails.kt
@@ -1,20 +1,26 @@
 package podlodka.crew.android.seven.dota.presentation
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.material.TextButton
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -22,6 +28,7 @@ internal fun GameDetails() {
     Column(
         verticalArrangement = Arrangement.spacedBy(24.dp),
         modifier = Modifier
+            .background(MaterialTheme.colors.background)
             .verticalScroll(rememberScrollState())
             .padding(24.dp)
     ) {
@@ -29,11 +36,13 @@ internal fun GameDetails() {
         Icon(
             imageVector = Icons.Default.ArrowBack,
             contentDescription = "Back button",
+            tint = MaterialTheme.colors.onBackground,
         )
 
         Icon(
             imageVector = Icons.Default.MoreVert,
             contentDescription = "More button",
+            tint = MaterialTheme.colors.onBackground,
         )
 
         Icon(
@@ -44,44 +53,81 @@ internal fun GameDetails() {
         Text(
             text = "DoTA 2",
             style = MaterialTheme.typography.h6,
+            color = MaterialTheme.colors.onBackground,
         )
 
-        Text("RATING")
+        Text(
+            text = "RATING",
+            color = MaterialTheme.colors.onBackground,
+        )
 
         Text(
             text = "70M",
             style = MaterialTheme.typography.caption,
+            color = Color(0xFF45454D),
         )
 
-        Text("TAGS")
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(10.dp),
+        ) {
+            repeat(5) {
+                Text(
+                    text = "TAG",
+                    color = MaterialTheme.colors.primaryVariant,
+                    modifier = Modifier
+                        .clip(CircleShape)
+                        .background(MaterialTheme.colors.primaryVariant)
+                        .padding(
+                            vertical = 5.dp,
+                            horizontal = 10.dp,
+                        )
+                )
+            }
+        }
 
         Text(
             text = "Dota 2 is a multiplayer online battle arena (MOBA) game which has two teams of five players compete to collectively destroy a large structure defended by the opposing team known as the \"Ancient\", whilst defending their own.",
             style = MaterialTheme.typography.caption,
+            color = Color(0xB3EEF2FB),
         )
 
-        Text("MEDIA")
+        Text(
+            text = "MEDIA",
+            color = MaterialTheme.colors.onBackground,
+        )
 
         Text(
             text = "Review & Ratings",
             style = MaterialTheme.typography.subtitle1,
+            color = MaterialTheme.colors.onBackground,
         )
 
         Text(
             text = "4.9",
             style = MaterialTheme.typography.h3,
+            color = MaterialTheme.colors.onBackground,
         )
 
-        Text("RATING")
+        Text(
+            text = "RATING",
+            color = MaterialTheme.colors.onBackground,
+        )
 
         Text(
             text = "70M Reviews",
             style = MaterialTheme.typography.caption,
+            color = Color(0xFFA8ADB7),
         )
 
-        Text("REVIEWS")
+        Text(
+            text = "REVIEWS",
+            color = MaterialTheme.colors.onBackground,
+        )
 
-        TextButton(
+        Button(
+            colors = ButtonDefaults.buttonColors(
+                backgroundColor = MaterialTheme.colors.secondary,
+            ),
             onClick = {
                 /*TODO*/
             },
@@ -89,6 +135,7 @@ internal fun GameDetails() {
             Text(
                 text = "Install",
                 style = MaterialTheme.typography.caption,
+                color = MaterialTheme.colors.onSecondary,
             )
         }
 
--- app/src/main/java/podlodka/crew/android/seven/dota/presentation/MainActivity.kt
+++ app/src/main/java/podlodka/crew/android/seven/dota/presentation/MainActivity.kt
@@ -3,13 +3,25 @@ package podlodka.crew.android.seven.dota.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.ui.graphics.Color
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            GameDetails()
+            MaterialTheme(
+                colors = darkColors(
+                    primary = Color(0xFF41A0E7),
+                    primaryVariant = Color(0x3D44A9F4),
+                    secondary = Color(0xFFF4D144),
+                    background = Color(0xFF050B18),
+                ),
+            ) {
+                GameDetails()
+            }
         }
     }
 
