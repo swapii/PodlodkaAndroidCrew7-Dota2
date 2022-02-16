f27e75104e025d9018f8d9ef4edc65702f02f63a
Pavel Savinov <swapii@gmail.com>
2022-02-16 13:38:49 +0300

    Apply bottom panel height as info bottom padding

--- app/src/main/java/podlodka/crew/android/seven/dota/presentation/GameDetails.kt
+++ app/src/main/java/podlodka/crew/android/seven/dota/presentation/GameDetails.kt
@@ -13,16 +13,25 @@ import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.boundsInParent
+import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
 
 @Composable
 internal fun GameDetails() {
 
-    Box {
+    Box(
+        modifier = Modifier
+            .background(MaterialTheme.colors.background)
+    ) {
 
         Image(
             painter = rememberImagePainter(data = "https://s3-alpha-sig.figma.com/img/284d/401f/f98fb91fbba05e15f99cdcd8e4f9d673?Expires=1646006400&Signature=OAGiVBdLRFBWxvG4UkCHI9QYZQ6UoC2Mo5nag0ZPMmDdPcCG6xKE9-TMgsHvWjfaauYmi8XSHiQyb3fRd5NXPuDWkw5ByMPqj7R97EDvFer8G9pZDXa~5rAMlPuqEggABRYR8zeOd~kKDJsvyyKeVGGUf90JyW6z7kwYZPIKmhfpard6R38-Y9ddX-xbzNTcPE5-7hRSobk3BtUMEJfz5UCKaSaQrZPpsHloNiXeAtI7RDQUuGMLWA5gMc6VFgTeZ4fl0I7K8M~qHIInOUzRbIDjl8gXFXXP0hpitPIfmX9A6vOVdxdjeel2nkQKLyv0ndeO6h3wsmec3WkB38R-0w__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"),
@@ -33,10 +42,15 @@ internal fun GameDetails() {
                 .height(300.dp)
         )
 
+        var bottomPanelHeight by remember { mutableStateOf(0f) }
+
         Info(
             modifier = Modifier
                 .verticalScroll(rememberScrollState())
-                .padding(top = 260.dp)
+                .padding(
+                    top = 260.dp,
+                    bottom = withDensity { bottomPanelHeight.toDp() },
+                )
         )
 
         Toolbar(
@@ -46,6 +60,9 @@ internal fun GameDetails() {
 
         Box(
             modifier = Modifier
+                .onGloballyPositioned {
+                    bottomPanelHeight = it.boundsInParent().height
+                }
                 .align(Alignment.BottomCenter)
                 .fillMaxWidth()
                 .background(MaterialTheme.colors.background)
---
+++ app/src/main/java/podlodka/crew/android/seven/dota/presentation/withDensity.kt
@@ -0,0 +1,9 @@
+package podlodka.crew.android.seven.dota.presentation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Density
+
+@Composable
+inline fun <T> withDensity(getValue: Density.() -> T): T =
+	LocalDensity.current.getValue()
